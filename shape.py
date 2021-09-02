f77b97af7f9ce27fceb83e5bdfc6674e36251010
Mohammad Essam <ongoingprodigy@gmail.com>
2021-09-02 19:08:54 -0400

    Fixing

--- shape.py
+++ shape.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
+import sys
 
-
-img = cv.imread('triangle.png')
+img = cv.imread(sys.argv[1])
 greyImage = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 _ ,thresholdImage  = cv.threshold(greyImage, 240 , 255, cv.CHAIN_APPROX_NONE)
 contours , _ = cv.findContours(thresholdImage, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
@@ -12,8 +12,8 @@ for contour in contours:
     cv.drawContours(img, [approx], 0, (0, 0, 0), 5)
     x = approx.ravel()[0] - 8
     y = approx.ravel()[1]
-    if len(approx) == 3:
-        cv.putText( img, "it Has " + str(len(approx)) +' side', (x, y), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255) )
+    cv.putText( img, "it Has " + str(len(approx)) +' side', (x, y), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255) )
+    print("it Has " + str(len(approx)) +' side')
 cv.imshow('shapes', img)
 Key = cv.waitKey(0)
 cv.destroyAllWindows()
